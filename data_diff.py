ede87b545131e22f4ec113b4f16f96528361c989
Guillermo Ricardo Moreno Carrillo <gmorenoc@ica-cluster.ica.ele.puc-rio.br>
2026-03-11 15:10:01 -0300

    docker

--- data_diff.py
+++ data_diff.py
@@ -6,6 +6,8 @@ import numpy as np
 import firedrake as fd
 from firedrake.petsc import PETSc
 
+from firedrake.pyplot import tricontourf
+
 if __name__ == '__main__':
     root = (os.path.join("data","2phase"))
     ds_list = os.listdir(os.path.join("data","2phase"))
@@ -13,4 +15,6 @@ if __name__ == '__main__':
     EI = extract_info(os.path.join(root,ds)) 
 
     #u, V, mesh = dataframe_to_firedrake(EI.df_f,['X','Y','Z'],'FR')
-    u, V, mesh = dataframe_to_firedrake(EI.df_f,['GridCentroidX','GridCentroidY'],'FR')
+    mesh, V, u = dataframe_to_firedrake(EI.df_f,['GridCentroidX','GridCentroidY'],'FR')
+
+    tricontourf(f_current_fd, levels=levels_f, axes=axes[0], cmap="inferno")
